3d5859da23f437bc8ea33ca64e269648850a8cb2
johannpino <johannpino@gmail.com>
2019-01-18 11:52:04 -0300

    Updates

---
+++ precache-manifest.a84947f0b1f045cde62c183c291b4041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceecadfc6bafaefaf667b729c7712296",
+    "url": "/shopping-cart-react-redux/static/media/banner.ceecadfc.jpg"
+  },
+  {
+    "revision": "bf6ea73a9a61949625ff",
+    "url": "/shopping-cart-react-redux/static/js/runtime~main.bf6ea73a.js"
+  },
+  {
+    "revision": "7156ed56af5e3cbf8a25",
+    "url": "/shopping-cart-react-redux/static/js/main.7156ed56.chunk.js"
+  },
+  {
+    "revision": "43ce49291e2efab5347d",
+    "url": "/shopping-cart-react-redux/static/js/1.43ce4929.chunk.js"
+  },
+  {
+    "revision": "7156ed56af5e3cbf8a25",
+    "url": "/shopping-cart-react-redux/static/css/main.9e60d2f5.chunk.css"
+  },
+  {
+    "revision": "d8c008441386341b7a8d8000bac839da",
+    "url": "/shopping-cart-react-redux/index.html"
+  }
+];
